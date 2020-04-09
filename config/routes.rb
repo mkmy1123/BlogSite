9b0f08a7561b0b9c68ca9b4a76baf0b7564068e1
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-04-09 22:00:06 +0900

    [Improve]ルート書き直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  root 'blogs#index'
   get 'about' => 'static_page#about'
   devise_for :users
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'blogs#index'
 end
